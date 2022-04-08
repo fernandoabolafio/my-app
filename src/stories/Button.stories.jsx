dc1e6c0d64125d0490ff6fc35ec73bb706a6a8d2
Fernando Abolafio <fernandohsabolafio@gmail.com>
2022-04-08 09:13:00 +0200

    change button story

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -19,6 +19,7 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
+  backgroundColor: 'red',
   label: 'Button',
 };
 
